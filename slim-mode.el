66b4c28d0c15595b959c8419c0c7f2ae7b3f011e
jordonbiondo <jordonbiondo@gmail.com>
2016-01-23 09:37:52 -0500

    rename symbols

--- slim-mode.el
+++ slim-mode.el
@@ -1,4 +1,4 @@
-;;; slim-mode.el --- Major mode for editing Slim files
+;;; emblem-mode.el --- Major mode for editing Slim files
 
 ;; Copyright (c) 2007, 2008 Nathan Weizenbaum
 ;; Copyright (c) 2009-2013 Daniel Mendler
@@ -7,7 +7,7 @@
 ;; Author: Nathan Weizenbaum
 ;; Author: Daniel Mendler
 ;; Author: Bozhidar Batsov
-;; URL: http://github.com/slim-template/emacs-slim
+;; URL: http://github.com/emblem-template/emacs-slim
 ;; Version: 1.1
 ;; Keywords: markup, language
 
@@ -21,7 +21,7 @@
 ;; your .emacs file:
 
 ;;
-;; (require 'slim-mode)
+;; (require 'emblem-mode)
 
 ;;; Code:
 
@@ -35,30 +35,30 @@
 (defgroup slim nil
   "Support for the Slim template language."
   :group 'languages
-  :prefix "slim-")
+  :prefix "emblem-")
 
-(defcustom slim-mode-hook nil
+(defcustom emblem-mode-hook nil
   "Hook run when entering Slim mode."
   :type 'hook
   :group 'slim)
 
-(defcustom slim-indent-offset 2
+(defcustom emblem-indent-offset 2
   "Amount of offset per level of indentation."
   :type 'integer
   :group 'slim)
 
-(defcustom slim-backspace-backdents-nesting t
-  "Non-nil to have `slim-electric-backspace' re-indent all code
+(defcustom emblem-backspace-backdents-nesting t
+  "Non-nil to have `emblem-electric-backspace' re-indent all code
 nested beneath the backspaced line be re-indented along with the
 line itself."
   :type 'boolean
   :group 'slim)
 
-(defvar slim-indent-function 'slim-indent-p
+(defvar emblem-indent-function 'emblem-indent-p
   "This function should look at the current line and return true
 if the next line could be nested within this line.")
 
-(defvar slim-block-openers
+(defvar emblem-block-openers
   `("^ *\\([\\.#a-z][^ \t]*\\)\\(\\[.*\\]\\)?"
     "^ *[-=].*do[ \t]*\\(|.*|[ \t]*\\)?$"
     ,(concat "^ *-[ \t]*\\("
@@ -74,7 +74,7 @@ text nested beneath them.")
 ;; Font lock
 
 ;; Helper for nested block (comment, embedded, text)
-(defun slim-nested-re (re)
+(defun emblem-nested-re (re)
   (concat "^\\( *\\)" re "\n\\(?:\\(?:\\1 .*\\)\n\\)*"))
 
 (defvar html-tags
@@ -95,15 +95,15 @@ text nested beneath them.")
 
 (defvar html-tags-re (concat "^ *\\(" (regexp-opt html-tags 'words) "\/?\\)"))
 
-(defconst slim-font-lock-keywords
+(defconst emblem-font-lock-keywords
   `(;; comment block
-    (,(slim-nested-re "/.*")
+    (,(emblem-nested-re "/.*")
      0 font-lock-comment-face)
     ;; embedded block
-    (,(slim-nested-re "\\([a-z0-9_]+:\\)")
+    (,(emblem-nested-re "\\([a-z0-9_]+:\\)")
      0 font-lock-preprocessor-face)
     ;; text block
-    (,(slim-nested-re "[\|'`].*")
+    (,(emblem-nested-re "[\|'`].*")
      0 font-lock-string-face)
     ;; directive
     ("^!.*"
@@ -150,21 +150,21 @@ text nested beneath them.")
     ("^ *[\\.#a-z0-9_-]+.*[^<>!]\\(==?'?\\) +"
      1 font-lock-preprocessor-face)))
 
-(defconst slim-embedded-re "^ *[a-z0-9_-]+:")
-(defconst slim-comment-re  "^ */")
+(defconst emblem-embedded-re "^ *[a-z0-9_-]+:")
+(defconst emblem-comment-re  "^ */")
 
-(defun* slim-extend-region ()
+(defun* emblem-extend-region ()
   "Extend the font-lock region to encompass embedded engines and comments."
   (let ((old-beg font-lock-beg)
         (old-end font-lock-end))
     (save-excursion
       (goto-char font-lock-beg)
       (beginning-of-line)
-      (unless (or (looking-at slim-embedded-re)
-                  (looking-at slim-comment-re))
-        (return-from slim-extend-region))
+      (unless (or (looking-at emblem-embedded-re)
+                  (looking-at emblem-comment-re))
+        (return-from emblem-extend-region))
       (setq font-lock-beg (point))
-      (slim-forward-sexp)
+      (emblem-forward-sexp)
       (beginning-of-line)
       (setq font-lock-end (max font-lock-end (point))))
     (or (/= old-beg font-lock-beg)
@@ -173,47 +173,47 @@ text nested beneath them.")
 
 ;; Mode setup
 
-(defvar slim-mode-syntax-table
+(defvar emblem-mode-syntax-table
   (let ((table (make-syntax-table)))
     (modify-syntax-entry ?: "." table)
     (modify-syntax-entry ?_ "w" table)
     table)
-  "Syntax table in use in slim-mode buffers.")
+  "Syntax table in use in emblem-mode buffers.")
 
-(defvar slim-mode-map
+(defvar emblem-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map "\177" 'slim-electric-backspace)
-    (define-key map "\C-?" 'slim-electric-backspace)
-    (define-key map "\C-c\C-f" 'slim-forward-sexp)
-    (define-key map "\C-c\C-b" 'slim-backward-sexp)
-    (define-key map "\C-c\C-u" 'slim-up-list)
-    (define-key map "\C-c\C-d" 'slim-down-list)
-    (define-key map "\C-c\C-k" 'slim-kill-line-and-indent)
+    (define-key map "\177" 'emblem-electric-backspace)
+    (define-key map "\C-?" 'emblem-electric-backspace)
+    (define-key map "\C-c\C-f" 'emblem-forward-sexp)
+    (define-key map "\C-c\C-b" 'emblem-backward-sexp)
+    (define-key map "\C-c\C-u" 'emblem-up-list)
+    (define-key map "\C-c\C-d" 'emblem-down-list)
+    (define-key map "\C-c\C-k" 'emblem-kill-line-and-indent)
     map))
 
 ;; For compatibility with Emacs < 24, derive conditionally
-(defalias 'slim-parent-mode
+(defalias 'emblem-parent-mode
   (if (fboundp 'prog-mode) 'prog-mode 'fundamental-mode))
 
 ;;;###autoload
-(define-derived-mode slim-mode slim-parent-mode "Slim"
+(define-derived-mode emblem-mode emblem-parent-mode "Slim"
   "Major mode for editing Slim files.
 
-\\{slim-mode-map}"
-  (set-syntax-table slim-mode-syntax-table)
-  (add-to-list 'font-lock-extend-region-functions 'slim-extend-region)
+\\{emblem-mode-map}"
+  (set-syntax-table emblem-mode-syntax-table)
+  (add-to-list 'font-lock-extend-region-functions 'emblem-extend-region)
   (set (make-local-variable 'font-lock-multiline) t)
-  (set (make-local-variable 'indent-line-function) 'slim-indent-line)
-  (set (make-local-variable 'indent-region-function) 'slim-indent-region)
+  (set (make-local-variable 'indent-line-function) 'emblem-indent-line)
+  (set (make-local-variable 'indent-region-function) 'emblem-indent-region)
   (set (make-local-variable 'parse-sexp-lookup-properties) t)
   (set (make-local-variable 'electric-indent-chars) nil)
   (setq comment-start "/")
   (setq indent-tabs-mode nil)
-  (setq font-lock-defaults '((slim-font-lock-keywords) nil t)))
+  (setq font-lock-defaults '((emblem-font-lock-keywords) nil t)))
 
 ;; Useful functions
 
-(defun slim-comment-block ()
+(defun emblem-comment-block ()
   "Comment the current block of Slim code."
   (interactive)
   (save-excursion
@@ -223,24 +223,24 @@ text nested beneath them.")
       (newline)
       (indent-to indent)
       (beginning-of-line)
-      (slim-mark-sexp)
-      (slim-reindent-region-by slim-indent-offset))))
+      (emblem-mark-sexp)
+      (emblem-reindent-region-by emblem-indent-offset))))
 
-(defun slim-uncomment-block ()
+(defun emblem-uncomment-block ()
   "Uncomment the current block of Slim code."
   (interactive)
   (save-excursion
     (beginning-of-line)
-    (while (not (looking-at slim-comment-re))
-      (slim-up-list)
+    (while (not (looking-at emblem-comment-re))
+      (emblem-up-list)
       (beginning-of-line))
-    (slim-mark-sexp)
+    (emblem-mark-sexp)
     (kill-line 1)
-    (slim-reindent-region-by (- slim-indent-offset))))
+    (emblem-reindent-region-by (- emblem-indent-offset))))
 
 ;; Navigation
 
-(defun slim-forward-through-whitespace (&optional backward)
+(defun emblem-forward-through-whitespace (&optional backward)
   "Move the point forward at least one line, until it reaches
 either the end of the buffer or a line with no whitespace.
 
@@ -251,7 +251,7 @@ If `backward' is non-nil, move the point backward instead."
           while (and (not (funcall endp))
                      (looking-at "^[ \t]*$")))))
 
-(defun slim-at-indent-p ()
+(defun emblem-at-indent-p ()
   "Returns whether or not the point is at the first
 non-whitespace character in a line or whitespace preceding that
 character."
@@ -260,7 +260,7 @@ character."
       (back-to-indentation)
       (>= (point) opoint))))
 
-(defun slim-forward-sexp (&optional arg)
+(defun emblem-forward-sexp (&optional arg)
   "Move forward across one nested expression.
 With `arg', do it that many times.  Negative arg -N means move
 backward across N balanced expressions.
@@ -269,18 +269,18 @@ A sexp in Slim is defined as a line of Slim code as well as any
 lines nested beneath it."
   (interactive "p")
   (or arg (setq arg 1))
-  (if (and (< arg 0) (not (slim-at-indent-p)))
+  (if (and (< arg 0) (not (emblem-at-indent-p)))
       (back-to-indentation)
     (while (/= arg 0)
       (let ((indent (current-indentation)))
-        (loop do (slim-forward-through-whitespace (< arg 0))
+        (loop do (emblem-forward-through-whitespace (< arg 0))
               while (and (not (eobp))
                          (not (bobp))
                          (> (current-indentation) indent)))
         (back-to-indentation)
         (setq arg (+ arg (if (> arg 0) -1 1)))))))
 
-(defun slim-backward-sexp (&optional arg)
+(defun emblem-backward-sexp (&optional arg)
   "Move backward across one nested expression.
 With ARG, do it that many times.  Negative arg -N means move
 forward across N balanced expressions.
@@ -288,46 +288,46 @@ forward across N balanced expressions.
 A sexp in Slim is defined as a line of Slim code as well as any
 lines nested beneath it."
   (interactive "p")
-  (slim-forward-sexp (if arg (- arg) -1)))
+  (emblem-forward-sexp (if arg (- arg) -1)))
 
-(defun slim-up-list (&optional arg)
+(defun emblem-up-list (&optional arg)
   "Move out of one level of nesting.
 With ARG, do this that many times."
   (interactive "p")
   (or arg (setq arg 1))
   (while (> arg 0)
     (let ((indent (current-indentation)))
-      (loop do (slim-forward-through-whitespace t)
+      (loop do (emblem-forward-through-whitespace t)
             while (and (not (bobp))
                        (>= (current-indentation) indent)))
       (setq arg (- arg 1))))
   (back-to-indentation))
 
-(defun slim-down-list (&optional arg)
+(defun emblem-down-list (&optional arg)
   "Move down one level of nesting.
 With ARG, do this that many times."
   (interactive "p")
   (or arg (setq arg 1))
   (while (> arg 0)
     (let ((indent (current-indentation)))
-      (slim-forward-through-whitespace)
+      (emblem-forward-through-whitespace)
       (when (<= (current-indentation) indent)
-        (slim-forward-through-whitespace t)
+        (emblem-forward-through-whitespace t)
         (back-to-indentation)
         (error "Nothing is nested beneath this line"))
       (setq arg (- arg 1))))
   (back-to-indentation))
 
-(defun slim-mark-sexp ()
+(defun emblem-mark-sexp ()
   "Marks the next Slim block."
-  (let ((forward-sexp-function 'slim-forward-sexp))
+  (let ((forward-sexp-function 'emblem-forward-sexp))
     (mark-sexp)))
 
-(defun slim-mark-sexp-but-not-next-line ()
+(defun emblem-mark-sexp-but-not-next-line ()
   "Marks the next Slim block, but puts the mark at the end of the
 last line of the sexp rather than the first non-whitespace
 character of the next line."
-  (slim-mark-sexp)
+  (emblem-mark-sexp)
   (let ((pos-of-end-of-line (save-excursion
                               (goto-char (mark))
                               (end-of-line)
@@ -342,26 +342,26 @@ character of the next line."
 
 ;; Indentation and electric keys
 
-(defun slim-indent-p ()
+(defun emblem-indent-p ()
   "Returns true if the current line can have lines nested beneath it."
-  (loop for opener in slim-block-openers
+  (loop for opener in emblem-block-openers
         if (looking-at opener) return t
         finally return nil))
 
-(defun slim-compute-indentation ()
+(defun emblem-compute-indentation ()
   "Calculate the maximum sensible indentation for the current line."
   (save-excursion
     (beginning-of-line)
     (if (bobp) 0
-      (slim-forward-through-whitespace t)
+      (emblem-forward-through-whitespace t)
       (+ (current-indentation)
-         (if (funcall slim-indent-function) slim-indent-offset
+         (if (funcall emblem-indent-function) emblem-indent-offset
            0)))))
 
-(defun slim-indent-region (start end)
+(defun emblem-indent-region (start end)
   "Indent each nonblank line in the region.
 This is done by indenting the first line based on
-`slim-compute-indentation' and preserving the relative
+`emblem-compute-indentation' and preserving the relative
 indentation of the rest of the region.
 
 If this command is used multiple times in a row, it will cycle
@@ -373,8 +373,8 @@ between possible indentations."
     (let (this-line-column current-column
           (next-line-column
            (if (and (equal last-command this-command) (/= (current-indentation) 0))
-               (* (/ (- (current-indentation) 1) slim-indent-offset) slim-indent-offset)
-             (slim-compute-indentation))))
+               (* (/ (- (current-indentation) 1) emblem-indent-offset) emblem-indent-offset)
+             (emblem-compute-indentation))))
       (while (< (point) end)
         (setq this-line-column next-line-column
               current-column (current-indentation))
@@ -391,26 +391,26 @@ between possible indentations."
         (forward-line 1)))
     (move-marker end nil)))
 
-(defun slim-indent-line ()
+(defun emblem-indent-line ()
   "Indent the current line.
 The first time this command is used, the line will be indented to the
 maximum sensible indentation.  Each immediately subsequent usage will
-back-dent the line by `slim-indent-offset' spaces.  On reaching column
+back-dent the line by `emblem-indent-offset' spaces.  On reaching column
 0, it will cycle back to the maximum sensible indentation."
   (interactive "*")
   (let ((ci (current-indentation))
         (cc (current-column))
-        (need (slim-compute-indentation)))
+        (need (emblem-compute-indentation)))
     (save-excursion
       (beginning-of-line)
       (delete-horizontal-space)
       (if (and (equal last-command this-command) (/= ci 0))
-          (indent-to (* (/ (- ci 1) slim-indent-offset) slim-indent-offset))
+          (indent-to (* (/ (- ci 1) emblem-indent-offset) emblem-indent-offset))
         (indent-to need)))
       (if (< (current-column) (current-indentation))
           (forward-to-indentation 0))))
 
-(defun slim-reindent-region-by (n)
+(defun emblem-reindent-region-by (n)
   "Add N spaces to the beginning of each line in the region.
 If N is negative, will remove the spaces instead.  Assumes all
 lines in the region have indentation >= that of the first line."
@@ -420,13 +420,13 @@ lines in the region have indentation >= that of the first line."
       (while (re-search-forward (concat "^" (make-string ci ? )) bound t)
         (replace-match (make-string (max 0 (+ ci n)) ? ) bound nil)))))
 
-(defun slim-electric-backspace (arg)
+(defun emblem-electric-backspace (arg)
   "Delete characters or back-dent the current line.
 If invoked following only whitespace on a line, will back-dent
 the line and all nested lines to the immediately previous
-multiple of `slim-indent-offset' spaces.
+multiple of `emblem-indent-offset' spaces.
 
-Set `slim-backspace-backdents-nesting' to nil to just back-dent
+Set `emblem-backspace-backdents-nesting' to nil to just back-dent
 the current line."
   (interactive "*p")
   (if (or (/= (current-indentation) (current-column))
@@ -436,28 +436,28 @@ the current line."
     (save-excursion
       (let ((ci (current-column)))
         (beginning-of-line)
-        (if slim-backspace-backdents-nesting
-            (slim-mark-sexp-but-not-next-line)
+        (if emblem-backspace-backdents-nesting
+            (emblem-mark-sexp-but-not-next-line)
           (set-mark (save-excursion (end-of-line) (point))))
-        (slim-reindent-region-by (* (- arg) slim-indent-offset))
+        (emblem-reindent-region-by (* (- arg) emblem-indent-offset))
         (back-to-indentation)
         (pop-mark)))))
 
-(defun slim-kill-line-and-indent ()
+(defun emblem-kill-line-and-indent ()
   "Kill the current line, and re-indent all lines nested beneath it."
   (interactive)
   (beginning-of-line)
-  (slim-mark-sexp-but-not-next-line)
+  (emblem-mark-sexp-but-not-next-line)
   (kill-line 1)
-  (slim-reindent-region-by (* -1 slim-indent-offset)))
+  (emblem-reindent-region-by (* -1 emblem-indent-offset)))
 
-(defun slim-indent-string ()
-  "Return the indentation string for `slim-indent-offset'."
-  (mapconcat 'identity (make-list slim-indent-offset " ") ""))
+(defun emblem-indent-string ()
+  "Return the indentation string for `emblem-indent-offset'."
+  (mapconcat 'identity (make-list emblem-indent-offset " ") ""))
 
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.slim\\'" . slim-mode))
+(add-to-list 'auto-mode-alist '("\\.slim\\'" . emblem-mode))
 
 ;; Setup/Activation
-(provide 'slim-mode)
-;;; slim-mode.el ends here
+(provide 'emblem-mode)
+;;; emblem-mode.el ends here
